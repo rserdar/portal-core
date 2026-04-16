e761d8539e02dd20d3237d234ce6791375a1413b
Serdar Yavuz <r.serdar@gmail.com>
2026-04-16 11:43:34 +0300

    Set custom SVG as primary favicon and PWA icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,7 @@ export default defineConfig({
         lang: 'tr',
         icons: [
           { src: 'pwa-64x64.png',           sizes: '64x64',   type: 'image/png' },
+          { src: 'favicon.svg',             sizes: 'any',     type: 'image/svg+xml' },
           { src: 'pwa-192x192.png',          sizes: '192x192', type: 'image/png' },
           { src: 'pwa-512x512.png',          sizes: '512x512', type: 'image/png' },
           { src: 'maskable-icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
